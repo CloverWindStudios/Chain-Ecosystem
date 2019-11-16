51eeafefd37a35624970e622fe8da76b4afda270
yunqili0809 <42676224+yunqili0809@users.noreply.github.com>
2019-11-16 12:41:30 -0500

    add plants

---
+++ Assets/Code/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool bunnyExist;
+    public bool birdExist;
+    public bool catExist;
+    public bool squirrleExist;
+
+    public double respawnTime;
+    public double lifeTimeWhole;
+    public double lifeTimeHalf;
+
+    public GameObject bunnyObj;
+    public GameObject birdObj;
+    public GameObject catObj;
+    public GameObject squirrleObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (bunnyExist)
+        {
+            Instantiate(bunnyObj);
+            Instantiate(bunnyObj);
+        }
+
+        if (birdExist)
+        {
+            Instantiate(birdObj);
+            Instantiate(birdObj);
+        }
+
+        if (catExist)
+        {
+            Instantiate(catObj);
+            Instantiate(catObj);
+        }
+
+        if (squirrleExist)
+        {
+            Instantiate(squirrleObj);
+            Instantiate(squirrleObj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
